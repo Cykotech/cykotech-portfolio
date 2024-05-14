946b8b14ffe45ee1362e289122bc9bf1fa8b9e74
Cykotech <cykotech.development@yahoo.com>
2024-05-14 12:54:36 -0400

    Update Dark Mode

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function darkModeToggle() {
-  return document.body.classList.toggle("dark");
-}
+const body = document.body;
+const darkModeSwitch = document.querySelector('#toggle');
 
 document.getElementById("toggle").addEventListener("click", () => {
-  return document.body.classList.toggle("dark");
+  if (darkModeSwitch.checked) {
+    body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
+  }
 });
+
+function checkInitialTheme() {
+  if (window.matchMedia("(prefers-color-scheme: dark)")) {
+    darkModeSwitch.checked = true;
+    body.classList.add("dark");
+  }
+}
+
+checkInitialTheme();
