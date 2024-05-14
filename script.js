ea05002a573e6450e3ab37874b4d721bd1feeaeb
Cykotech <cykotech.development@yahoo.com>
2024-05-14 14:23:28 -0400

    Add Image cycle

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.body;
-const darkModeSwitch = document.querySelector('#toggle');
+const darkModeSwitch = document.querySelector("#toggle");
+const lms = document.querySelector("#lms");
 
 document.getElementById("toggle").addEventListener("click", () => {
   if (darkModeSwitch.checked) {
@@ -17,3 +18,23 @@ function checkInitialTheme() {
 }
 
 checkInitialTheme();
+
+const lmsImages = ["./resources/totk_main.png", "./resources/totk_inventory.png", "./resources/totk_armor.png"];
+
+function changeLmsImage(delay, iteration) {
+  lms.style.backgroundImage = `url(${lmsImages[iteration]})`;
+
+  console.log(iteration);
+
+  if (iteration < lmsImages.length) {
+    setTimeout(() => changeLmsImage(delay, iteration + 1), delay);
+  }
+
+  if (iteration === lmsImages.length) {
+    lms.style.backgroundImage = `url(${lmsImages[0]})`;
+  }
+}
+
+lms.addEventListener("mouseenter", () => {
+  changeLmsImage(2000, 0);
+});
