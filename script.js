b313dde3152fdd0fd379426734a6ef252abf05ed
Cykotech <cykotech.development@yahoo.com>
2024-08-11 20:00:10 -0400

    Update to v2

--- script.js
+++ script.js
@@ -1,40 +1,96 @@
-const body = document.body;
-const darkModeSwitch = document.querySelector("#toggle");
-const lms = document.querySelector("#lms");
+const menuButtons = document.querySelectorAll("header div.text-content ul li");
+const scroller = document.querySelector(".scroller");
+const nav = document.querySelector("nav");
+const aboutButtons = document.querySelectorAll(".about-button");
+const projectsButtons = document.querySelectorAll(".projects-button");
+const contactButtons = document.querySelectorAll(".contact-button");
+const aboutSection = document.querySelector("#about");
+const projectsSection = document.querySelector("#projects");
+const contactSection = document.querySelector("#contact");
+const returnButton = document.querySelector(".top-button");
 
-document.getElementById("toggle").addEventListener("click", () => {
-  if (darkModeSwitch.checked) {
-    body.classList.add("dark");
-  } else {
-    body.classList.remove("dark");
+function preLoadTransition() {
+  menuButtons.forEach((button) => {
+    button.offsetHeight;
+
+    button.classList.remove("pre-load");
+  });
+}
+
+setTimeout(() => {
+  preLoadTransition();
+}, 250);
+
+function addAnimation() {
+  scroller.setAttribute("data-animated", true);
+
+  const scrollerInner = scroller.querySelector(".scroller-inner");
+  const scrollerContent = Array.from(scrollerInner.children);
+
+  scrollerContent.forEach((item) => {
+    const duplicatedItem = item.cloneNode(true);
+    duplicatedItem.setAttribute("aria-hidden", true);
+
+    scrollerInner.appendChild(duplicatedItem);
+  });
+
+  const skills = document.querySelectorAll(".skills li");
+
+  skills.forEach((skill) => {
+    const span = skill.querySelector("span");
+    const p = skill.querySelector("p");
+    const img = skill.querySelector("img");
+
+    skill.addEventListener("mouseenter", () => {
+      span.style.border = "1px solid transparent";
+      p.style.opacity = 1;
+      img.style.transform = "rotate(-45deg) scale(120%)";
+    });
+
+    skill.addEventListener("mouseleave", () => {
+      span.style.border = "1px solid hsla(0, 0%, 56%, 0.5)";
+      p.style.opacity = 0;
+      img.style.transform = "rotate(-45deg) scale(100%)";
+    });
+  });
+}
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+window.addEventListener("scroll", () => {
+  const height = window.innerHeight;
+
+  if (window.scrollY >= height) {
+    nav.classList.add("show");
+    returnButton.classList.add("show");
+  }
+
+  if (window.scrollY < height) {
+    nav.classList.remove("show");
+    returnButton.classList.remove("show");
   }
 });
 
-function checkInitialTheme() {
-  if (window.matchMedia("(prefers-color-scheme: dark)")) {
-    darkModeSwitch.checked = true;
-    body.classList.add("dark");
-  }
-}
+aboutButtons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    window.scroll(0, aboutSection.offsetTop);
+  });
+});
 
-checkInitialTheme();
+projectsButtons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    window.scroll(0, projectsSection.offsetTop);
+  });
+});
 
-const lmsImages = ["./resources/totk_main.png", "./resources/totk_inventory.png", "./resources/totk_armor.png"];
+contactButtons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    window.scroll(0, contactSection.offsetTop);
+  });
+});
 
-function changeLmsImage(delay, iteration) {
-  lms.style.backgroundImage = `url(${lmsImages[iteration]})`;
-
-  console.log(iteration);
-
-  if (iteration < lmsImages.length) {
-    setTimeout(() => changeLmsImage(delay, iteration + 1), delay);
-  }
-
-  if (iteration === lmsImages.length) {
-    lms.style.backgroundImage = `url(${lmsImages[0]})`;
-  }
-}
-
-lms.addEventListener("mouseenter", () => {
-  changeLmsImage(2000, 0);
+returnButton.addEventListener("mousedown", () => {
+  window.scroll(0, 0);
 });
